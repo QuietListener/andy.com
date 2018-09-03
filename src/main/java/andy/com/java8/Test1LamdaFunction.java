3eca6f65e5b7eee8d512a9b8f44b7edf4557de78
junjun <junjun@baicizhan.com>
2018-09-03 15:38:40 +0800

    fx

--- src/main/java/andy/com/java8/Test1LamdaFunction.java
+++ src/main/java/andy/com/java8/Test1LamdaFunction.java
@@ -6,8 +6,8 @@ public class Test1LamdaFunction {
 
 	public static void main(String[] args)
 	{
-//		//可以将函数，或者代码块 当成一个参数传进去方法
-//		Arrays.asList( "a", "b", "d" ).forEach( ( String e ) -> System.out.println( e ) );
-//		System.out.println("00");
+		//可以将函数，或者代码块 当成一个参数传进去方法
+		Arrays.asList( "a", "b", "d" ).forEach( ( String e ) -> System.out.println( e ) );
+		System.out.println("00");
 	}
 }
--- src/main/java/andy/com/thrift/helloworld/MultiplicationHandler.java
+++ src/main/java/andy/com/thrift/helloworld/MultiplicationHandler.java
@@ -6,6 +6,7 @@ public class MultiplicationHandler implements MultiplicationService.Iface{
 
 	public int multiply(int n1, int n2) throws TException {
 		
+		System.out.println("receive "+n1+"+"+n2);
 		return n1 * n2;
 	}
 
