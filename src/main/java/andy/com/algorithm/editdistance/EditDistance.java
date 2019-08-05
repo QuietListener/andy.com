dd373d20789998a884af366b58e3d3682223132e
junjun <junjun@baicizhan.com>
2019-08-05 16:59:54 +0800

    fx

--- src/main/java/andy/com/algorithm/editdistance/EditDistance.java
+++ src/main/java/andy/com/algorithm/editdistance/EditDistance.java
@@ -8,7 +8,7 @@ import java.util.Collections;
  * 插入一个字符：ab -> abj
  * 删除一个字符：abj -> aj
  * 替换一个字符：axj -> ayj
- *
+ * <p>
  * 递归是从后往前的计算，
  * 动态规划是从前往后的计算
  */
@@ -25,10 +25,10 @@ public class EditDistance {
      * @return
      */
     public int editDistance1(char[] a, char[] b, int i, int j) {
-        if (i == 0) {
-            return j;
-        } else if (j == 0) {
-            return i;
+        if (i == 0 || i == -1) {
+            return j+1;
+        } else if (j == 0 || j == -1) {
+            return i+1;
         } else if (a[i] == b[j]) {
             //相同的
             return editDistance1(a, b, i - 1, j - 1);
@@ -55,32 +55,38 @@ public class EditDistance {
         int alength = a.length;
         int blength = b.length;
 
+        if (alength == 0) {
+            return blength;
+        } else if (blength == 0) {
+            return alength;
+        }
+
         int[][] matrix = new int[alength][blength];
 
         for (int i = 0; i < blength; i++) {
             matrix[0][i] = i;
         }
 
-        for(int j = 0; j < alength;j++){
+        for (int j = 0; j < alength; j++) {
             matrix[j][0] = j;
         }
 
-        for(int i = 1; i < alength;i++){
-            for(int j = 1; j < blength; j++)
-                if(a[i] == b[j]){
-                    matrix[i][j] = matrix[i-1][j-1];
-                }else{
-                    matrix[i][j] =  Collections.min(Arrays.asList(
+        for (int i = 1; i < alength; i++) {
+            for (int j = 1; j < blength; j++)
+                if (a[i] == b[j]) {
+                    matrix[i][j] = matrix[i - 1][j - 1];
+                } else {
+                    matrix[i][j] = Collections.min(Arrays.asList(
                             //+1是删除a[i]
-                            matrix[i-1][j]+1,
+                            matrix[i - 1][j] + 1,
                             //+1插入b[j]
-                            matrix[i][j-1]+1 ,
+                            matrix[i][j - 1] + 1,
                             //+1 是a[i]替换为b[j]
-                            matrix[i-1][j-1]+1));
+                            matrix[i - 1][j - 1] + 1));
                 }
         }
 
-        return matrix[alength-1][blength-1];
+        return matrix[alength - 1][blength - 1];
     }
 
 
@@ -94,31 +100,38 @@ public class EditDistance {
         int alength = a.length;
         int blength = b.length;
 
+
+        if (alength == 0) {
+            return blength;
+        } else if (blength == 0) {
+            return alength;
+        }
+
         int[][] matrix = new int[2][blength];
 
-        for(int i = 0; i < alength;i++){
-            for(int j = 1; j < blength; j++) {
-               if(i == 0){
-                   matrix[0][j] = j;
-               }else{
-                   matrix[i%2][j] =  Collections.min(Arrays.asList(
-                           //+1是删除a[i]
-                           matrix[(i-1)%2][j]+1,
-                           //+1插入b[j]
-                           matrix[i%2][j-1]+1 ,
-                           //+1 是a[i]替换为b[j]
-                           matrix[(i-1)%2][j-1]+1));
-               }
+        for (int i = 0; i < alength; i++) {
+            for (int j = 1; j < blength; j++) {
+                if (i == 0) {
+                    matrix[0][j] = j;
+                } else {
+                    matrix[i % 2][j] = Collections.min(Arrays.asList(
+                            //+1是删除a[i]
+                            matrix[(i - 1) % 2][j] + 1,
+                            //+1插入b[j]
+                            matrix[i % 2][j - 1] + 1,
+                            //+1 是a[i]替换为b[j]
+                            matrix[(i - 1) % 2][j - 1] + 1));
+                }
             }
         }
 
-        return matrix[alength%2][blength-1];
+        return matrix[alength % 2][blength - 1];
     }
 
 
     public static void main(String args[]) {
         EditDistance ed = new EditDistance();
-        char[] a = "aaaabbb".toCharArray();
+        char[] a = "".toCharArray();
         char[] b = "aaaa".toCharArray();
 
         int ed1 = ed.editDistance1(a, b, a.length - 1, b.length - 1);
