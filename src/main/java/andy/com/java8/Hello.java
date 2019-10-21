315294d2e4df332a5632f683cd8c4fa09a0738cd
junjun <junjun@baicizhan.com>
2019-10-21 17:58:36 +0800

    fx

--- src/main/java/andy/com/java8/Hello.java
+++ src/main/java/andy/com/java8/Hello.java
@@ -1,8 +1,18 @@
 package andy.com.java8;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("----s");
+		test("a","b");
+	}
+
+	public static void test(String ... keys){
+		String str = Stream.of(keys).collect(Collectors.joining(","));
+		System.out.println(str);
 	}
 }
--- src/main/java/andy/com/stuff/Test.java
+++ src/main/java/andy/com/stuff/Test.java
@@ -1,6 +1,8 @@
 package andy.com.stuff;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
     static public void main(String [] args){
@@ -10,5 +12,9 @@ public class Test {
         Date d = new Date(1560762692963l);
         System.out.println(d);
 
+        Map a = new HashMap<Integer,Integer>();
+        a.put(1,1);
+        a.put(2,1);
+        System.out.println(a.toString());
     }
 }
