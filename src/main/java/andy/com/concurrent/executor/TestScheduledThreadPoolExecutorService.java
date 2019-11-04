3a8fba1cc633d69c70174a4f146cf35989ca0c2a
junjun <junjun@baicizhan.com>
2019-11-04 18:30:29 +0800

    fx

--- src/main/java/andy/com/concurrent/executor/TestScheduledThreadPoolExecutorService.java
+++ src/main/java/andy/com/concurrent/executor/TestScheduledThreadPoolExecutorService.java
@@ -1,5 +1,8 @@
 package andy.com.concurrent.executor;
 
+import org.junit.Test;
+
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -28,44 +31,61 @@ public class TestScheduledThreadPoolExecutorService {
 
 	//线程池
 	static ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(2,new ScheduledThreadPoolExecutor.DiscardPolicy());
-	
-	public static void main(String[] args) throws InterruptedException 
-	{	
+
+
+
+	@Test
+	public void test1(){
+
 		for(int i = 0; i < 40; i++)
 		{
-			pool.schedule(new Task("task"+i),500,TimeUnit.MILLISECONDS);
-			System.out.println("add task "+i);
+			long now = System.currentTimeMillis();
+			int delay = new Random().nextInt(1000)+1000;
+			Task t = new Task("task:"+now+":"+delay);
+			pool.schedule(t,delay,TimeUnit.MILLISECONDS);
+			System.out.println("add task:"+t.name);
 		}
-		
-		//调用shutdown之后不能有新的任务再加入了
-		pool.shutdown();
-		
-		//在shutdown收调用会抛出 java.util.concurrent.RejectedExecutionException:
-		//pool.execute(new Task("task"+44));
-		
-		//当调用shutdown之后, 阻塞直到所有任务结束, 或者阻塞指定的时间
-		pool.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
-		
-		System.out.println("all thread complete");
+
+		close();
 	}
-	
+
+	static void close(){
+		try {
+			//调用shutdown之后不能有新的任务再加入了
+			pool.shutdown();
+
+			//在shutdown收调用会抛出 java.util.concurrent.RejectedExecutionException:
+			//pool.execute(new Task("task"+44));
+
+			//当调用shutdown之后, 阻塞直到所有任务结束, 或者阻塞指定的时间
+			pool.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+
+			System.out.println("all thread complete");
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+
 	static  class Task implements  Runnable
 	{
 		private String name = null;
 		public Task(String name) {
 			this.name = name;
 		}
-		
+
 		public void run(){
-		
-				try {
-					TimeUnit.MILLISECONDS.sleep(50);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			
-			System.out.println(this.name +" finished");
-			
+
+			try {
+				TimeUnit.MILLISECONDS.sleep(50);
+				long interval = System.currentTimeMillis() - Long.parseLong(this.name.split(":")[1]);
+				System.out.println(this.name +" finished interval:"+interval);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+
+
 		}
 	}
 }
