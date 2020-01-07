ac4f9c6bd0633339bf3e7407b2eb3d2ebb963bc7
junjun <junjun@baicizhan.com>
2020-01-07 17:04:53 +0800

    fx

---
+++ src/main/java/andy/com/web/stuff/ImageUtil.java
@@ -0,0 +1,133 @@
+package andy.com.web.stuff;
+
+
+import org.junit.Test;
+import sun.misc.BASE64Encoder;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
+
+public class ImageUtil {
+
+    static BASE64Encoder encoder = new sun.misc.BASE64Encoder();
+
+    // 验证码字符集
+    private static final String[] chars = {
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "人", "们", "百", "日", "美", "团",
+            "的", "智", "能", "联", "网", "配", "送", "柜", "可", "以", "将", "餐", "食", "交", "付", "给",
+            "通", "过", "手", "机", "应", "用", "预", "定", "外", "卖", "的", "顾", "客", "无", "人", "配",
+            "送", "车", "能", "够", "自", "主", "穿", "行", "在", "拥", "挤", "而", "复", "杂", "的", "城",
+            "市", "交", "通", "环", "境"};
+
+    // 字符数量
+    private static final int SIZE = 4;
+    // 干扰线数量
+    private static final int LINES = 10;
+    // 宽度
+    private static final int WIDTH = 120;
+    // 高度
+    private static final int HEIGHT = 60;
+    // 字体大小
+    private static final int FONT_SIZE = 30;
+
+    /**
+     * 生成随机验证码及图片
+     * Object[0]：验证码字符串；
+     * Object[1]：验证码图片。
+     */
+    public static Object[] createImage() {
+        StringBuffer sb = new StringBuffer();
+        // 1.创建空白图片
+        BufferedImage image = new BufferedImage(
+                WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        // 2.获取图片画笔
+        Graphics graphic = image.getGraphics();
+        // 3.设置画笔颜色
+        graphic.setColor(Color.LIGHT_GRAY);
+        // 4.绘制矩形背景
+        graphic.fillRect(0, 0, WIDTH, HEIGHT);
+        // 5.画随机字符
+        Random ran = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            // 取随机字符索引
+            int n = ran.nextInt(chars.length);
+            // 设置随机颜色
+            graphic.setColor(getRandomColor());
+            // 设置字体大小
+            graphic.setFont(new Font(
+                    null, Font.BOLD + Font.ITALIC, FONT_SIZE / 2 + 5 + new Random().nextInt(FONT_SIZE / 2)));
+            // 画字符
+            graphic.drawString(
+                    chars[n] + "", i * WIDTH / SIZE, HEIGHT * 1 / 3 + new Random().nextInt(HEIGHT * 2 / 3));
+            // 记录字符
+            sb.append(chars[n]);
+        }
+        // 6.画干扰线
+        for (int i = 0; i < LINES; i++) {
+            // 设置随机颜色
+            graphic.setColor(getRandomColor());
+            // 随机画线
+            graphic.drawLine(ran.nextInt(WIDTH), ran.nextInt(HEIGHT),
+                    ran.nextInt(WIDTH), ran.nextInt(HEIGHT));
+        }
+        // 7.返回验证码和图片
+        return new Object[]{sb.toString(), image};
+    }
+
+    /**
+     * 随机取色
+     */
+    public static Color getRandomColor() {
+        Random ran = new Random();
+        Color color = new Color(ran.nextInt(256),
+                ran.nextInt(256), ran.nextInt(256));
+        return color;
+    }
+
+
+    public static String[] createBase64Image() throws IOException {
+        Object[] objs = createImage();
+        BufferedImage image = (BufferedImage) objs[1];
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(image, "jpg", baos);
+            byte[] bytes = baos.toByteArray();
+            String ret = encoder.encodeBuffer(bytes).trim();
+            return new String[]{(String) (objs[0]), ret};
+        } finally {
+            baos.close();
+            image = null;
+            baos = null;
+        }
+    }
+
+    @Test
+    public void test1() throws IOException {
+        Object[] objs = createImage();
+        BufferedImage image = (BufferedImage) objs[1];
+        OutputStream os = new FileOutputStream("1.png");
+        ImageIO.write(image, "png", os);
+        os.close();
+    }
+
+    @Test
+    public void test2() throws IOException {
+        Object[] objs = createBase64Image();
+        System.out.println(String.format("%s  ,  %s", objs[0], objs[1]));
+    }
+
+}
+
+
+
