acc8dcb22982a9c57f16750d6d901002252b9fd2
junjun <junjun@baicizhan.com>
2020-10-19 10:16:58 +0800

    fx

--- src/main/java/andy/com/nio/buffer/protocal/Frame.java
+++ src/main/java/andy/com/nio/buffer/protocal/Frame.java
@@ -2,10 +2,14 @@ package andy.com.nio.buffer.protocal;
 
 import andy.com.nio.buffer.protocal.compress.Compressor;
 import andy.com.nio.buffer.protocal.compress.GzipCompressor;
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.apache.commons.lang.ArrayUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 消息头:
@@ -23,12 +27,22 @@ public class Frame {
 
     byte[] head = new byte[6];
     byte[] data = null;
+    /**
+     * 数据原始大小 包括头
+     */
+    private int originFrameLength = -1;
+
+    /**
+     * 压缩后的大小 包括头
+     */
+    private int comprredFrameLength = -1;
 
     private Compressor cgzip = new GzipCompressor();
 
-    private Frame() {}
+    private Frame() {
+    }
 
-    private Frame(String s,byte compressType ) throws IOException {
+    private Frame(String s, byte compressType) throws IOException {
 
         head[0] = 1; //type1 data;
         head[1] = 1; //version 1
@@ -36,15 +50,20 @@ public class Frame {
         head[3] = 0; //保留
 
         data = s.getBytes();
-        System.out.println("data.length = "+data.length);
-        if(head[2] != COMPRESS_NONE){
-            if(head[2] == COMPRESS_GZIP){
+        this.originFrameLength = data.length + head.length;
+        System.out.println("data.length = " + data.length);
+
+        if (head[2] != COMPRESS_NONE) {
+            if (head[2] == COMPRESS_GZIP) {
                 data = cgzip.compress(data);
-                System.out.println("cdata.length = "+data.length);
+                System.out.println("cdata.length = " + data.length);
             }
         }
 
         int length = data.length;
+
+        this.comprredFrameLength = this.head.length + data.length;
+
         byte[] lbytes = PUtils.intToByteArray(length);
 
         //长度2个字节
@@ -62,44 +81,92 @@ public class Frame {
         return data;
     }
 
-
-    public static Frame encode(String s) throws IOException{
-        return new Frame(s,COMPRESS_GZIP);
+    public int getOriginFrameLength() {
+        return originFrameLength;
     }
 
-    public static Frame decode(byte [] bs) throws IOException{
-        Frame f = new Frame();
-        f.head = Arrays.copyOfRange(bs,0,6);
-        byte[] lengthBytes = new byte[]{0,0,f.head[4],f.head[5]};
-        int length = PUtils.byteArrayToInt(lengthBytes);
-        f.data = Arrays.copyOfRange(bs,HEAD_LENGTH,length+HEAD_LENGTH);
+    public int getComprredFrameLength() {
+        return comprredFrameLength;
+    }
 
-        if(f.head[2] != COMPRESS_NONE){
-            if(f.head[2] == COMPRESS_GZIP){
+    public void setComprredFrameLength(int comprredFrameLength) {
+        this.comprredFrameLength = comprredFrameLength;
+    }
+
+    public static Frame encode(String s) throws IOException {
+        return new Frame(s, COMPRESS_GZIP);
+    }
+
+    public static Frame decode(byte[] bs) throws IOException, FrameNeedMoreBytesException {
+        Frame f = new Frame();
+        f.head = Arrays.copyOfRange(bs, 0, 6);
+        byte[] lengthBytes = new byte[]{0, 0, f.head[4], f.head[5]};
+        int length = PUtils.byteArrayToInt(lengthBytes);
+
+        if (length + HEAD_LENGTH > bs.length) {
+            throw new FrameNeedMoreBytesException();
+        }
+
+        f.data = Arrays.copyOfRange(bs, HEAD_LENGTH, length + HEAD_LENGTH);
+        f.comprredFrameLength = f.head.length + f.data.length;
+
+        if (f.head[2] != COMPRESS_NONE) {
+            if (f.head[2] == COMPRESS_GZIP) {
                 f.data = f.cgzip.uncompress(f.data);
             }
         }
+        f.originFrameLength = f.head.length + f.data.length;
+
         return f;
     }
 
-    public static void main(String[] args) throws  Exception {
-        String a = "abcdefghiaaaaaaadddddddddddddddddddddddddsssssssssssssssssssssssslalallalaaaaaaaaaaaaaaalallalalalldddddddddj1123";
+    public static void main(String[] args) throws Exception {
 
-        Frame f = Frame.encode(a);
+        List<String> strs = Arrays.asList("aaaaaaad322222222222222222ddd", "bbb2323bdccc", "eeee11212345");
 
-        byte [] h = f.getHead();
-        byte [] d = f.getData();
+        int total_length = 0;
+        ByteOutputStream bo = new ByteOutputStream();
+        for (int i = 0; i < strs.size(); i++) {
+            Frame ff = Frame.encode(strs.get(i));
+            bo.write(ff.head);
+            bo.write(ff.data);
+            total_length += ff.head.length;
+            total_length += ff.data.length;
+        }
 
-        byte[] interupt = new byte[]{1,2,2,4,5};
 
-        byte [] both = ArrayUtils.addAll(h,d);
-        both = ArrayUtils.addAll(both,interupt);
+        byte[] bs = bo.getBytes();
+        ByteInputStream bi = new ByteInputStream(bs, total_length);
 
 
-        Frame f1 = Frame.decode(both);
-        String a1 = new String(f1.getData());
+        byte[] buf = new byte[20];
+        byte[] databuf = null;
+        int size = 0;
+        List<Frame> fs = new ArrayList<>();
+        while ((size = bi.read(buf)) > 0) {
+            if (databuf == null) {
+                databuf = Arrays.copyOfRange(buf, 0, size);
+            } else {
+                databuf = ArrayUtils.addAll(databuf, buf);
+            }
 
-        System.out.println(a.equals(a1));
+            try {
+                Frame f1 = Frame.decode(databuf);
+                int length = f1.getComprredFrameLength();
+                fs.add(f1);
+                databuf = Arrays.copyOfRange(databuf, length, databuf.length);
+            } catch (FrameNeedMoreBytesException e) {
+                System.out.println("need more");
+            }
+        }
+
+
+        for (int i = 0; i < fs.size(); i++) {
+            Frame f2 = fs.get(i);
+            String s = new String(f2.getData());
+            System.out.println(s + ":" + s.equals(strs.get(i)));
+        }
+
 
     }
 }
---
+++ src/main/java/andy/com/nio/buffer/protocal/FrameNeedMoreBytesException.java
@@ -0,0 +1,7 @@
+package andy.com.nio.buffer.protocal;
+
+public class FrameNeedMoreBytesException extends Exception {
+    public FrameNeedMoreBytesException(){
+        super();
+    }
+}
